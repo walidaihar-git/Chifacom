fd2e957be1cc78b8a8d40553722b4e67aa3fbd49
AkramYns <akramyounes17@gmail.com>
2021-01-21 22:31:20 +0100

    signup update

---
+++ android/Chifacom/app/src/main/java/serive/algeria/chifacom/register/Register.java
@@ -0,0 +1,16 @@
+package serive.algeria.chifacom.register;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+import serive.algeria.chifacom.R;
+
+public class Register extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_register2);
+    }
+}
