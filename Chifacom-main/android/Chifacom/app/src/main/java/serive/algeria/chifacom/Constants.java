288c06039d2a2fe85bfade2761d7a20ca5ec6956
AkramYns <akramyounes17@gmail.com>
2021-02-08 03:10:41 +0100

    salam

---
+++ Chifacom-main/android/Chifacom/app/src/main/java/serive/algeria/chifacom/Constants.java
@@ -0,0 +1,14 @@
+package serive.algeria.chifacom;
+
+
+
+public class Constants {
+    private static final String ROOT_URL =
+            "http://192.168.1.3/chifacom/scripts/";
+
+    public static final String URL_ROGISTER = ROOT_URL + "RegisterUser.php";
+    public static final String URL_LOGIN = ROOT_URL + "UserLogin.php";
+
+
+
+}
---
+++ Chifacom-main/android/Chifacom/app/src/main/java/serive/algeria/chifacom/doctor/TimeLineFragment.java
@@ -0,0 +1,96 @@
+package serive.algeria.chifacom.doctor;
+
+import android.app.Dialog;
+import android.os.Bundle;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.AdapterView;
+import android.widget.ImageButton;
+import android.widget.ListView;
+import android.widget.TextView;
+import android.widget.Toast;
+
+import androidx.appcompat.widget.AppCompatButton;
+import androidx.fragment.app.Fragment;
+
+import java.util.ArrayList;
+import java.util.Calendar;
+import java.util.List;
+
+import serive.algeria.chifacom.R;
+import serive.algeria.chifacom.register.ThirdStepFragment;
+
+
+public class TimeLineFragment extends Fragment {
+
+
+    private ListView listView ;
+    private List<Patient> list ;
+    private Patient patient ;
+    private AdapterPatient adapterPatient;
+
+
+    public TimeLineFragment() {
+        // Required empty public constructor
+    }
+
+    public static TimeLineFragment newInstance() {
+        TimeLineFragment fragment = new TimeLineFragment();
+
+        return fragment;
+    }
+
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+
+    }
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+        // Inflate the layout for this fragment
+        View view= inflater.inflate(R.layout.fragment_time_line, container, false);
+
+        listView = view.findViewById(R.id.list_view);
+        setPatient();
+
+
+
+        /*listView.setOnItemClickListener(new AdapterView.OnItemClickListener() {
+            @Override
+            public void onItemClick(AdapterView<?> parent, View view, int position, long id) {
+                Toast.makeText(getActivity(),position+"", Toast.LENGTH_SHORT).show();
+
+            }
+        });*/
+        return view;
+    }
+
+
+
+    private void setPatient() {
+        patient = new Patient();
+
+        list = new ArrayList<Patient>();
+        adapterPatient = new AdapterPatient(getActivity(),R.layout.item_patient_timeline,list);
+
+        list.add(new Patient(1,"Younes Akram","22","06977974132","Gripe ..."));
+        list.add(new Patient(2,"Younes Abdelmoula","20","06777912332","حالة مستعجلة للجلد"));
+        list.add(new Patient(3,"Yahia Bilal","24","07998974132","استشارة فقط"));
+        list.add(new Patient(4,"Abdou","22","05667974132","زكام"));
+        list.add(new Patient(1,"Younes Akram","22","06977974132","Gripe ..."));
+        list.add(new Patient(1,"Younes Akram","22","06977974132","Gripe ..."));
+        list.add(new Patient(1,"Younes Akram","22","06977974132","Gripe ..."));
+        list.add(new Patient(1,"Younes Akram","22","06977974132","Gripe ..."));
+        list.add(new Patient(1,"Younes Akram","22","06977974132","Gripe ..."));
+        list.add(new Patient(1,"Younes Akram","22","06977974132","Gripe ..."));
+        list.add(new Patient(1,"Younes Akram","22","06977974132","Gripe ..."));
+        list.add(new Patient(1,"Younes Akram","22","06977974132","Gripe ..."));
+
+
+
+        listView.setAdapter(adapterPatient);
+    }
+}
