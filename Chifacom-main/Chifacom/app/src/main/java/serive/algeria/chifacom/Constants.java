9ef5da7321e8144b3e321767e653d4f262f02e89
walidaihar-git <walidaihar@gmail.com>
2021-02-25 14:06:28 +0100

    Update Constants.java

--- Chifacom-main/Chifacom/app/src/main/java/serive/algeria/chifacom/Constants.java
+++ Chifacom-main/Chifacom/app/src/main/java/serive/algeria/chifacom/Constants.java
@@ -4,7 +4,11 @@ package serive.algeria.chifacom;
 
 public class Constants {
     private static final String ROOT_URL =
+<<<<<<< Updated upstream
             "http://192.168.1.3/chifacom/scripts/";
+=======
+            "http://192.168.137.187/chifacom/scripts/";
+>>>>>>> Stashed changes
 
     public static final String URL_ROGISTER = ROOT_URL + "RegisterUser.php";
     public static final String URL_LOGIN = ROOT_URL + "UserLogin.php";
